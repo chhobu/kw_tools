9ff4a95ca6601e3bfbe3d32d5b2e4079ae607686
chhobu <buchho@gmail.com>
2013-01-02 17:47:05 +0100

    Rappatriement des fonctionnalités du module collapsiblock

---
+++ js/kw_block.js
@@ -0,0 +1,62 @@
+Drupal.behaviors.kBlock = {attach: function(context, settings) {(function ($) {
+  var getCookieData = function () {
+    if ($.cookie) {
+      var cookieString = $.cookie('collapsiblock');
+      return cookieString ? $.parseJSON(cookieString) : {};
+    }
+    else {
+      return '';
+    }
+  };
+  $('.block.collapsible').not('.collapsible-processed').each(function() {
+    $title = $(this).find('h2');
+    blockId = $(this).attr('id');
+    if ($title) {
+      $title.wrapInner('<a href="#" class="collapse-link">');
+    }
+    $(this).addClass('collapsible-processed');
+    items = getCookieData();
+    collapse = 0;
+    if (items[blockId] == 0) {
+      $(this).addClass('collapsed').removeClass('expanded');
+      collapse = 1;
+    }
+    else if (items[blockId] == 1) {
+      $(this).removeClass('collapsed').addClass('expanded');
+    }
+    else if ($(this).hasClass('collapsed')) {
+      collapse = 1;
+    }
+    if (collapse) {
+      $(this).find('.content').hide();
+    }
+  });
+  $('.block.collapsible .collapse-link').click(function() {
+    $block = $(this).parents('.block');
+    $content = $block.find('.content').removeClass('animated').stop();
+    if ($block.hasClass('expanded')) {
+      status = 0;
+      $content.removeAttr('style').addClass('animated').slideUp('fast', function() {
+        $(this).removeClass('animated');
+        $block.addClass('collapsed').removeClass('expanded');
+      });
+    }
+    else {
+      status = 1;
+      $block.removeClass('collapsed').addClass('expanded');
+      $content.addClass('animated').slideDown('fast', function() {
+        $(this).removeClass('animated');
+      });
+    }
+    items = getCookieData();
+    items[$block.attr('id')] = status;
+    var cookieString = '{ ';
+    var cookieParts = [];
+    $.each(items, function(id, setting) {
+      cookieParts[cookieParts.length] = ' "' + id + '": ' + setting;
+    });
+    cookieString += cookieParts.join(', ') + ' }';
+    $.cookie('collapsiblock', cookieString, {path: settings.basePath});
+    return false;
+  });
+})(jQuery);}};
